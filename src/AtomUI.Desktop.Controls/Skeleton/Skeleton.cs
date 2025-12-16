447b0c56a09f9a2e47b2c63c2f21862426660f27
chinboy <qinware@163.com>
2025-12-16 21:40:16 +0800

    Add Card content alignment

--- src/AtomUI.Desktop.Controls/Skeleton/Skeleton.cs
+++ src/AtomUI.Desktop.Controls/Skeleton/Skeleton.cs
@@ -6,6 +6,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
+using Avalonia.Layout;
 using Avalonia.LogicalTree;
 using Avalonia.Metadata;
 
@@ -56,6 +57,12 @@ public class Skeleton : AbstractSkeleton, IControlSharedTokenResourcesHost
     
     public static readonly StyledProperty<IDataTemplate?> ContentTemplateProperty = 
         ContentControl.ContentTemplateProperty.AddOwner<Skeleton>();
+    
+    public static readonly StyledProperty<HorizontalAlignment> HorizontalContentAlignmentProperty =
+        ContentControl.HorizontalContentAlignmentProperty.AddOwner<Skeleton>();
+    
+    public static readonly StyledProperty<VerticalAlignment> VerticalContentAlignmentProperty =
+        ContentControl.VerticalContentAlignmentProperty.AddOwner<Skeleton>();
     
     public bool IsLoading
     {
@@ -141,6 +148,18 @@ public class Skeleton : AbstractSkeleton, IControlSharedTokenResourcesHost
     {
         get => GetValue(ContentTemplateProperty);
         set => SetValue(ContentTemplateProperty, value);
+    }
+    
+    public HorizontalAlignment HorizontalContentAlignment
+    {
+        get => GetValue(HorizontalContentAlignmentProperty);
+        set => SetValue(HorizontalContentAlignmentProperty, value);
+    }
+    
+    public VerticalAlignment VerticalContentAlignment
+    {
+        get => GetValue(VerticalContentAlignmentProperty);
+        set => SetValue(VerticalContentAlignmentProperty, value);
     }
     #endregion
 
